ddd475480308b40458e621ed2bb7404f6ee8d58f
icecreamdonut <snakeowner@gmail.com>
2015-03-17 18:21:05 +0000

    blah

--- server.py
+++ server.py
@@ -1,25 +1,36 @@
 import os
 import uuid
+import psycopg2
+import psycopg2.extras
 from flask import Flask, session
-from flask.ext.socketio import SocketIO, emit
+from flask.ext.socketio import SocketIO, emit, request
 
 app = Flask(__name__, static_url_path='')
-app.config['SECRET_KEY'] = 'secret!'
-
+app.config['SECRET_KEY'] = 'secrbkkjbkhbkhbet!'
+app.debug = True
 socketio = SocketIO(app)
 
-messages = [{'text':'test', 'name':'testName'}]
+messages = [{'text':'test', 'name':'testName'}
 users = {}
 
+
+def db_connect():
+    connectionString = 'dbname=chat user=postgres password=gohan host=localhost'
+    try:
+        return psycopg2.connect(connectionString)
+    except:
+        print("Can't connect to database")
+        
+        
 def updateRoster():
     names = []
     for user_id in  users:
-        print users[user_id]['username']
+        #print users[user_id]['username']
         if len(users[user_id]['username'])==0:
             names.append('Anonymous')
         else:
             names.append(users[user_id]['username'])
-    print 'broadcasting names'
+    #print 'broadcasting names'
     emit('roster', names, broadcast=True)
     
 
@@ -31,30 +42,72 @@ def test_connect():
     
     users[session['uuid']]={'username':'New User'}
     updateRoster()
+    
+    conn = db_connect()
+    cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+    query = "select users.username, message from messages join users on users.id = messages.username"
+    cur.execute(query)
+    results = cur.fetchall()
+    
 
-
-    for message in messages:
-        emit('message', message)
+    for result in results:
+        
+        print (result)
+        result = {'name': result['username'], 'text':result['message']}
+        emit('message', result)
 
 @socketio.on('message', namespace='/chat')
 def new_message(message):
     #tmp = {'text':message, 'name':'testName'}
+    
     tmp = {'text':message, 'name':users[session['uuid']]['username']}
     messages.append(tmp)
+    #query = "insert into messages (username, message) values ('
+    conn = db_connect()
+    cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+    cur.execute("""INSERT INTO messages VALUES (default,%s, %s);""", 
+    (session['id'], message))
+    conn.commit()
+    
     emit('message', tmp, broadcast=True)
+    
+    
+@socketio.on('search', namespace='/chat')
+def search(search):
+    db =db_connect()
+    cur = db.cursor(cursor_factory=psycopg2.extras.DictCursor)
+    query = "select users.username, message from messages join users on users.id = messages.username where message like %s"
+    cur.execute(query,(search,))
+    results = cur.fetchall()
+    
+    for result in results:
+        result = {'name': result['username'], 'textSearch':result['message']}
+        emit('search', result, broadcast=True)
     
 @socketio.on('identify', namespace='/chat')
 def on_identify(message):
-    print 'identify' + message
+    #print 'identify' + message
     users[session['uuid']]={'username':message}
     updateRoster()
 
 
 @socketio.on('login', namespace='/chat')
-def on_login(pw):
-    print 'login '  + pw
-    #users[session['uuid']]={'username':message}
-    #updateRoster()
+def on_login(data):
+    #print 'login '  + pw
+    db = db_connect()
+    cur = db.cursor(cursor_factory=psycopg2.extras.DictCursor)
+    #print "HI"
+    username = data['username']
+    password = data['password']
+    #zipcode = request.form['zipcode']
+    query = "select * from users where username = %s and password = crypt(%s,password)"
+    cur.execute(query, (username, password))
+    result = cur.fetchone()
+    if result:
+        users[session['uuid']]={'username':data['username']}
+        session['username'] =data['username']
+        session['id'] = result['id']
+        updateRoster()
 
 
     
